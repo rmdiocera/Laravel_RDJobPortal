107da53b5c5c94c02f07f6c6a96165917136b3fd
Reginald Diocera <rmdiocera@gmail.com>
2020-04-12 22:04:35 +0800

    Added model for applicant info, degree and gender

---
+++ database/migrations/2020_04_11_145010_create_applicant_infos_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicantInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicant_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('age');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('country');
+            $table->string('nationality');
+            $table->string('job_title');
+            $table->string('company_name');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('salary');
+            $table->mediumText('tasks');
+            $table->string('university');
+            $table->string('degree');
+            $table->string('course');
+            $table->date('univ_start_date');
+            $table->date('univ_end_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applicant_infos');
+    }
+}
