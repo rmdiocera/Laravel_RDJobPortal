c5aeab5d10080e582df4344d8dfe4d943155235d
Reginald Diocera <rmdiocera@gmail.com>
2020-04-21 22:47:01 +0800

    Modified active_applications and saved_jobs

--- resources/views/active_applications.blade.php
+++ resources/views/active_applications.blade.php
@@ -3,15 +3,19 @@
 @section('content')
     <h1>Active Applications</h1>
     @if (count($applications) > 0)
-        <div class="row d-flex justify-content-around">
+        <div class="row">
             @foreach ($applications as $application)
-            <div class="card col-md-5 py-2">
-                <h3>{{$application->title}}</h3>
-                <h5>{{$application->company_name}}</h5>
-                <span>{{$application->status}}</span>
-                <p>{{App\Http\Controllers\HomeController::getJobPostApplicantsCount($application->job_post_id, $application->comp_id)}} Applicant(s)</p>
-                <p>Applied on {{ Carbon\Carbon::parse($application->created_at)->format('F j, Y')}}</p>
-                <a href="/job-post/{{$application->id}}/withdraw"><button class="btn btn-sm btn-primary" type="button" >Withdraw Application</button></a>
+            <div class="col-md-6 py-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">{{$application->title}}</h3>
+                        <h5 class="card-title">{{$application->company_name}}</h5>
+                        <span>{{$application->status}}</span>
+                        <p class="card-text">{{App\Http\Controllers\HomeController::getJobPostApplicantsCount($application->job_post_id, $application->comp_id)}} Applicant(s)</p>
+                        <p class="card-text">Applied on {{ Carbon\Carbon::parse($application->created_at)->format('F j, Y')}}</p>
+                        <a href="/job-post/{{$application->id}}/withdraw"><button class="btn btn-sm btn-primary" type="button" >Withdraw Application</button></a>
+                    </div>
+                </div>
             </div>
             @endforeach
         </div>
--- resources/views/saved_jobs.blade.php
+++ resources/views/saved_jobs.blade.php
@@ -3,13 +3,17 @@
 @section('content')
     <h1>Saved Job Posts</h1>
     @if (count($job_posts) > 0)
-        <div class="row d-flex justify-content-around">
+        <div class="row">
             @foreach ($job_posts as $job_post)
-            <div class="card col-md-5 py-2">
-                <h3>{{$job_post->title}}</h3>
-                <h5>{{$job_post->company_name}}</h5>
-                <p>Saved on {{ Carbon\Carbon::parse($job_post->created_at)->format('F j, Y')}}</p>
-                <a href="/job-post/{{$job_post->id}}/unsave"><button class="btn btn-sm btn-primary" type="button" >Unsave Job Post</button></a>
+            <div class="col-md-6 py-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">{{$job_post->title}}</h3>
+                        <h5 class="card-title">{{$job_post->company_name}}</h5>
+                        <p class="card-text">Saved on {{ Carbon\Carbon::parse($job_post->created_at)->format('F j, Y')}}</p>
+                        <a href="/job-post/{{$job_post->id}}/unsave"><button class="btn btn-sm btn-primary" type="button" >Unsave Job Post</button></a>
+                    </div>
+                </div>
             </div>
             @endforeach
         </div>
