7524c365dd110222710f228038d9ceb5a8f6bc4f
Reginald Diocera <rmdiocera@gmail.com>
2020-04-26 18:52:12 +0800

    Added modals.blade as include file

---
+++ resources/views/inc/modals.blade.php
@@ -0,0 +1,17 @@
+<div id="actionsModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Confirm Delete</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body"></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary modal-btn">OK</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
