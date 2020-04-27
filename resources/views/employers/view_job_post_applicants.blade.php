99decf3405ff65902b0e0cb3a78963f16d74d6e4
Reginald Diocera <rmdiocera@gmail.com>
2020-04-27 23:29:49 +0800

    AJAX feature for invite/reject application

--- resources/views/employers/view_job_post_applicants.blade.php
+++ resources/views/employers/view_job_post_applicants.blade.php
@@ -49,15 +49,17 @@
                     <td class="d-flex justify-content-center">
                         <a href="/storage/app_resume/{{$applicant->resume}}" download><button class="btn btn-sm btn-primary" type="button">Download</button></a>
                     </td>
-                    <td>{{$applicant->status}}</td>
+                    <td data-status-id="{{$applicant->id}}">{{$applicant->status}}</td>
                     <td class="d-flex justify-content-around">
-                        {!! Form::open(['action' => ['EmployersController@inviteApplicantToInterview', $applicant->id], 'method' => 'PUT']) !!}
+                        {{-- {!! Form::open(['action' => ['EmployersController@inviteApplicantToInterview', $applicant->id], 'method' => 'PUT']) !!}
                             {{Form::submit('Invite to Interview', ['class' => 'btn btn-sm btn-success'])}}
-                        {!! Form::close() !!}
-                        {!! Form::open(['action' => ['EmployersController@rejectApplicantApplication', $applicant->id], 'method' => 'PUT']) !!}
+                        {!! Form::close() !!} --}}
+                        <button data-jp-app-id="{{$applicant->id}}" class="btn btn-sm btn-primary invite-app" type="button">Invite to Interview</button>
+                        {{-- {!! Form::open(['action' => ['EmployersController@rejectApplicantApplication', $applicant->id], 'method' => 'PUT']) !!}
                             {{Form::submit('Reject Application', ['class' => 'btn btn-sm btn-danger'])}}
-                        {!! Form::close() !!}
-                        {{-- <a href="/job-post/{{$applicant->id}}/invite"><button class="btn btn-sm btn-success" type="button" >Save Job Post</button></a> --}}
+                        {!! Form::close() !!} --}}
+                        <button data-jp-app-id="{{$applicant->id}}" class="btn btn-sm btn-danger reject-app" type="button">Reject Application</button>
+                        {{-- <a href="/job-post/{{$applicant->id}}/invite"><button class="btn btn-sm btn-success" type="button">Save Job Post</button></a> --}}
                         {{-- <a href="/job-post/{{$applicant->id}}/reject"><button class="btn btn-sm btn-danger" type="button">Apply to Job Post</button></a> --}}
                     </td>
                 </tr>
