e8c80238827eed2846ce6d98347d85ebf5c79232
Reginald Diocera <rmdiocera@gmail.com>
2020-04-27 23:29:25 +0800

    AJAX feature for accept/decline interview

--- resources/views/active_applications.blade.php
+++ resources/views/active_applications.blade.php
@@ -10,23 +10,25 @@
                     <div class="card-body">
                         <h3 class="card-title">{{$application->title}}</h3>
                         <h5 class="card-title">{{$application->company_name}}</h5>
-                        <span>{{$application->status}}</span>
+                        <span data-status-id="{{$application->id}}">{{$application->status}}</span>
                         <p class="card-text">{{App\Http\Controllers\HomeController::getJobPostApplicantsCount($application->job_post_id, $application->comp_id)}} Applicant(s)</p>
                         <p class="card-text">Applied on {{ Carbon\Carbon::parse($application->created_at)->format('F j, Y')}}</p>
                         <div class="form-inline mb-2">
-                            @if ($application->app_status_id === 2)
-                                {!! Form::open(['action' => ['HomeController@acceptInterviewInvitation', $application->id], 'method' => 'PUT']) !!}
+                            {{-- @if ($application->app_status_id === 2) --}}
+                                {{-- {!! Form::open(['action' => ['HomeController@acceptInterviewInvitation', $application->id], 'method' => 'PUT']) !!}
                                     {{Form::submit('Accept Interview Invitation', ['class' => 'btn btn-sm btn-success'])}}
-                                {!! Form::close() !!}
-                                {!! Form::open(['action' => ['HomeController@declineInterviewInvitation', $application->id], 'method' => 'PUT']) !!}
+                                {!! Form::close() !!} --}}
+                                <button data-jp-app-id="{{$application->id}}" class="btn btn-sm btn-success accept-int" type="button">Accept Interview Invitation</button>
+                                {{-- {!! Form::open(['action' => ['HomeController@declineInterviewInvitation', $application->id], 'method' => 'PUT']) !!}
                                     {{Form::submit('Reject Interview Invitation', ['class' => 'btn btn-sm btn-danger ml-2'])}}
-                                {!! Form::close() !!}
-                            @endif
+                                {!! Form::close() !!} --}}
+                                <button data-jp-app-id="{{$application->id}}" class="btn btn-sm btn-danger ml-2 decline-int" type="button">Decline Interview Invitation</button>
+                            {{-- @endif --}}
                             {{-- {!! Form::open(['action' => ['HomeController@removeApplicantJobPostApplication', $application->id], 'method' => 'DELETE']) !!}
                                 {{Form::submit('Withdraw Application', ['class' => 'btn btn-sm btn-danger'])}}
                             {!! Form::close() !!} --}}
-                            <button data-jp-app-id="{{$application->id}}" class="btn btn-sm btn-danger withdraw-jp" type="button" >Withdraw Application</button>
-                            {{-- <a href="/job-post/{{$application->id}}/withdraw"><button class="btn btn-sm btn-primary" type="button" >Withdraw Application</button></a> --}}
+                            <button data-jp-app-id="{{$application->id}}" class="btn btn-sm btn-danger ml-2 withdraw-jp" type="button">Withdraw Application</button>
+                            {{-- <a href="/job-post/{{$application->id}}/withdraw"><button class="btn btn-sm btn-primary" type="button">Withdraw Application</button></a> --}}
                         </div>                            
                     </div>
                 </div>
