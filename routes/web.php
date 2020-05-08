4509fa88e8b19244855e802e86aaa07c3365e656
Reginald Diocera <rmdiocera@gmail.com>
2020-05-08 23:39:49 +0800

    Added route names to job-search and job-posts

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ use Illuminate\Support\Facades\Session;
 Route::get('/', 'PagesController@index')->name('site.main');
 Route::get('/about', 'PagesController@about')->name('site.about_us');
 Route::get('/contact-us', 'PagesController@contactUs')->name('site.contact_us');
-Route::get('/job-search', 'PagesController@showJobPosts');
+Route::get('/job-search', 'PagesController@showJobPosts')->name('site.job_search');
 Route::get('/emp-info/{id}', 'PagesController@viewCompanyProfile');
 
 // Job Posts
-Route::get('/job-posts', 'JobPostsController@index');
+Route::get('/job-posts', 'JobPostsController@index')->name('employer.job_posts');
 Route::get('/job-post/{id}', 'JobPostsController@show');
 Route::get('/create-job-post', 'JobPostsController@create');
 Route::post('/job-search', 'JobPostsController@store');
