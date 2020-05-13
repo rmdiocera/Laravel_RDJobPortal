5c049d133d3c543b330f35d55bd624d59d98651a
Reginald Diocera <rmdiocera@gmail.com>
2020-05-13 22:34:11 +0800

    Added validation for failed login + lockout event

--- app/Http/Controllers/Auth/EmployerLoginController.php
+++ app/Http/Controllers/Auth/EmployerLoginController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Validation\ValidationException;
 use Auth;
 
 class EmployerLoginController extends Controller
 {
+    use ThrottlesLogins;
+
     public function __construct()
     {
         $this->middleware('guest:employer', ['except' => ['logout']]);   
@@ -21,6 +26,8 @@ class EmployerLoginController extends Controller
 
             if ($url_prev == url()->route('site.job_search')) {
                 session(['url.intended' => url()->route('employer.job_posts')]);   
+            } else if ($url_prev == url()->route('site.main').'/') {
+                session(['url.intended' => url()->route('employer.dashboard')]);
             } else {
                 session(['url.intended' => url()->previous()]);
             }
@@ -29,19 +36,47 @@ class EmployerLoginController extends Controller
         return view('auth.employer-login');
     }
 
+    public function sendFailedLoginResponse()
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
     public function login(Request $request)
     {
-        $this->validate($request, [
+        $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
 
         if (Auth::guard('employer')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
             return redirect()->intended(route('employer.dashboard'));
         }
 
-        return redirect()->back()->withInput($request->only('email', 'remember'));
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse();
+        // return redirect()->back()->with('error','These credentials do not match anything in our site.')->withInput($request->only('email', 'remember'));
         
+    }
+
+    public function username()
+    {
+        return 'email';
     }
 
     public function logout()
