c54d00fbf26f863a21e1ae87e35f6983a66374b1
Reginald Diocera <rmdiocera@gmail.com>
2020-05-13 22:42:34 +0800

    IDs for fadeOut + session('error') to ('warning')

--- resources/views/inc/alerts.blade.php
+++ resources/views/inc/alerts.blade.php
@@ -1,20 +1,20 @@
 @if (count($errors) > 0)
     @foreach ($errors->all() as $error)
-        <div id="" class="alert alert-danger">
+        <div class="alert alert-danger" id="alert-danger">
             {{$error}}
         </div>
     @endforeach
 @endif
 
 @if (session('success'))
-    <div class="alert alert-success">
+    <div class="alert alert-success" id="alert-success">
         {{session('success')}}
     </div>
 @endif
 
-@if (session('error'))
-    <div class="alert alert-warning">
-        {{session('error')}}
+@if (session('warning'))
+    <div class="alert alert-warning" id="alert-warning">
+        {{session('warning')}}
     </div>
 @endif
 
