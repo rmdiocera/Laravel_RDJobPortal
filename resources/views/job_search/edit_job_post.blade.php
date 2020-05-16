620b16e91b74e032c83b10683b63db7fd548f455
Reginald Diocera <rmdiocera@gmail.com>
2020-05-16 23:16:44 +0800

    Inserted if statement inside option + icon on save

--- resources/views/job_search/edit_job_post.blade.php
+++ resources/views/job_search/edit_job_post.blade.php
@@ -7,12 +7,13 @@
             <div class="form-group">
                 {{Form::label('industry', 'Industry')}}
                 <select name="industry" id="">
-                    @foreach ($data['industries'] as $industry)
-                        @if ($industry->id === $data['job_post']['industry_id'])
+                    @foreach ($data['industries'] as $industry)                    
+                        <option value="{{$industry->id}}" @if ($industry->id === $data['job_post']['industry_id']) selected @endif>{{$industry->industry}}</option>
+                        {{-- @if ($industry->id === $data['job_post']['industry_id'])
                             <option value="{{$industry->id}}" selected>{{$industry->industry}}</option>    
                         @else
                             <option value="{{$industry->id}}">{{$industry->industry}}</option>
-                        @endif
+                        @endif --}}
                     @endforeach
                 </select>
             </div>
@@ -20,23 +21,25 @@
                 {{Form::label('emp_type', 'Employment Type')}}
                 <select name="emp_type" id="">
                     @foreach ($data['emp_types'] as $emp_type)
-                        @if ($emp_type->id === $data['job_post']['emp_type_id'])
+                        <option value="{{$emp_type->id}}" @if ($emp_type->id === $data['job_post']['emp_type_id']) selected @endif>{{$emp_type->emp_type}}</option>
+                        {{-- @if ($emp_type->id === $data['job_post']['emp_type_id'])
                             <option value="{{$emp_type->id}}" selected>{{$emp_type->emp_type}}</option>
                         @else
                             <option value="{{$emp_type->id}}">{{$emp_type->emp_type}}</option>
-                        @endif
+                        @endif --}}
                     @endforeach
                 </select>
             </div>
             <div class="form-group">
                 {{Form::label('level', 'Job Level')}}
                 <select name="level" id="">
-                    @foreach ($data['levels'] as $level)
-                        @if ($level->id === $data['job_post']['level_id']) 
+                    @foreach ($data['levels'] as $level) 
+                        <option value="{{$level->id}}" @if ($level->id === $data['job_post']['level_id']) selected @endif>{{$level->job_level}}</option>
+                        {{-- @if ($level->id === $data['job_post']['level_id']) 
                             <option value="{{$level->id}}" selected>{{$level->job_level}}</option>
                         @else
                             <option value="{{$level->id}}">{{$level->job_level}}</option>
-                        @endif
+                        @endif --}}
                     @endforeach
                 </select>
             </div>
@@ -47,6 +50,7 @@
             {{Form::label('description', 'Description')}}
             {{Form::textarea('description', $data['job_post']['desc'], ['id' => 'multi_editor', 'class' => 'form-control', 'placeholder' => 'Enter the description here', 'rows' => 20])}}
         </div>
-        {{Form::submit('Submit', ['class' => 'btn btn-primary', 'value' => 'Post'])}}
+        {{Form::button('<i class="fas fa-check-circle mr-1"></i>Save', ['type' => 'submit', 'class' => 'btn btn-sm btn-primary'])}}
+        {{-- {{Form::submit('Submit', ['class' => 'btn btn-primary', 'value' => 'Post'])}} --}}
     {!! Form::close() !!}
 @endsection
