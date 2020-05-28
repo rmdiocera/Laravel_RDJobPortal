c2131a0ba1ffddf518bc67ef51f0e34651782ac2
Reginald Diocera <rmdiocera@gmail.com>
2020-05-28 23:11:21 +0800

    Added sticky footer to fix footer to bottom

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,24 +1,32 @@
-<footer class="pt-4 pt-md-5 border-top">
-    <div class="row">
-        <div class="col-12 col-md">
-            <span class="display-4 title-font">WorkForce</span>
-            <small class="d-block mb-3 text-muted">&copy; 2020</small>
+<div class="d-flex flex-column sticky-footer-wrapper">
+    <nav>
+    </nav>
+    <main class="flex-fill">
+    </main>
+    <footer class="pt-4 pt-md-5 border-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <span class="display-4 title-font">WorkForce</span>
+                    <small class="d-block mb-3 text-muted">&copy; 2020</small>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Contact Us</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><span class="text-muted"><i class="fab fa-linkedin mr-1"></i>linkedin.com/in/rmdiocera</span></li>
+                        <li><span class="text-muted"><i class="fas fa-envelope-square mr-1"></i>rmdiocera@gmail.com</span></li>
+                        <li><span class="text-muted"><i class="fab fa-github mr-1 align-middle"></i>github.com/rmdiocera</span></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">About This Website</a></li>
+                        <li><a class="text-muted" href="#">Privacy Policy</a></li>
+                        <li><a class="text-muted" href="#">Terms of Use</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="col-6 col-md">
-            <h5>Contact Us</h5>
-            <ul class="list-unstyled text-small">
-                <li><span class="text-muted"><i class="fab fa-linkedin mr-1"></i>linkedin.com/in/rmdiocera</span></li>
-                <li><span class="text-muted"><i class="fas fa-envelope-square mr-1"></i>rmdiocera@gmail.com</span></li>
-                <li><span class="text-muted"><i class="fab fa-github mr-1 align-middle"></i>github.com/rmdiocera</span></li>
-            </ul>
-        </div>
-        <div class="col-6 col-md">
-            <h5>About</h5>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">About This Website</a></li>
-                <li><a class="text-muted" href="#">Privacy Policy</a></li>
-                <li><a class="text-muted" href="#">Terms of Use</a></li>
-            </ul>
-        </div>
-    </div>
-</footer>
+    </footer>
+</div>
