3fadf598cd69807cf4c530ddc23fbbf36124b87f
Reginald Diocera <rmdiocera@gmail.com>
2020-05-31 22:38:21 +0800

    Added bootstrap-select on select elements

--- resources/views/job_search/create_job_post.blade.php
+++ resources/views/job_search/create_job_post.blade.php
@@ -5,31 +5,31 @@
     {!! Form::open(['action' => 'JobPostsController@store', 'method' => 'POST']) !!}
         <div class="form-inline">
             <div class="form-group">
-                {{Form::label('industry', 'Industry')}}
-                <select name="industry" id="">
+                {{-- {{Form::label('industry', 'Industry')}} --}}
+                <select name="industry" id="" title="Industry" class="selectpicker border rounded border-secondary">
                     @foreach ($data['industries'] as $industry)
-                        <option value="{{$industry->id}}">{{$industry->industry}}</option>
+                        <option value="{{$industry->id}}" @if (old('industry') == $industry->id) selected="selected" @endif>{{$industry->industry}}</option>
                     @endforeach
                 </select>
             </div>
             <div class="form-group ml-2">
-                {{Form::label('emp_type', 'Employment Type')}}
-                <select name="emp_type" id="">
+                {{-- {{Form::label('emp_type', 'Employment Type')}} --}}
+                <select name="emp_type" id="" title="Employment Type" class="selectpicker border rounded border-secondary">
                     @foreach ($data['emp_types'] as $emp_type)
-                        <option value="{{$emp_type->id}}">{{$emp_type->emp_type}}</option>
+                        <option value="{{$emp_type->id}}" @if (old('emp_type') == $emp_type->id) selected="selected" @endif>{{$emp_type->emp_type}}</option>
                     @endforeach
                 </select>
             </div>
             <div class="form-group ml-2">
-                {{Form::label('level', 'Job Level')}}
-                <select name="level" id="">
+                {{-- {{Form::label('level', 'Job Level')}} --}}
+                <select name="level" id="" title="Job Level" class="selectpicker border rounded border-secondary">
                     @foreach ($data['levels'] as $level)
-                        <option value="{{$level->id}}">{{$level->job_level}}</option>
+                        <option value="{{$level->id}}" @if (old('level') == $level->id) selected="selected" @endif>{{$level->job_level}}</option>
                     @endforeach
                 </select>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group my-2">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Enter your job title here'])}}
             {{Form::label('description', 'Description')}}
--- resources/views/job_search/edit_job_post.blade.php
+++ resources/views/job_search/edit_job_post.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create Job Post</h1>
+    <h1>Edit Job Post</h1>
     {!! Form::open(['action' => ['JobPostsController@update', $data['job_post']['id']], 'method' => 'PUT']) !!}
         <div class="form-inline">
             <div class="form-group">
-                {{Form::label('industry', 'Industry')}}
-                <select name="industry" id="">
+                {{-- {{Form::label('industry', 'Industry')}} --}}
+                <select name="industry" id="" class="selectpicker border rounded border-secondary">
                     @foreach ($data['industries'] as $industry)                    
                         <option value="{{$industry->id}}" @if ($industry->id === $data['job_post']['industry_id']) selected @endif>{{$industry->industry}}</option>
                         {{-- @if ($industry->id === $data['job_post']['industry_id'])
@@ -17,9 +17,9 @@
                     @endforeach
                 </select>
             </div>
-            <div class="form-group">
-                {{Form::label('emp_type', 'Employment Type')}}
-                <select name="emp_type" id="">
+            <div class="form-group ml-2">
+                {{-- {{Form::label('emp_type', 'Employment Type')}} --}}
+                <select name="emp_type" id="" class="selectpicker border rounded border-secondary">
                     @foreach ($data['emp_types'] as $emp_type)
                         <option value="{{$emp_type->id}}" @if ($emp_type->id === $data['job_post']['emp_type_id']) selected @endif>{{$emp_type->emp_type}}</option>
                         {{-- @if ($emp_type->id === $data['job_post']['emp_type_id'])
@@ -30,9 +30,9 @@
                     @endforeach
                 </select>
             </div>
-            <div class="form-group">
-                {{Form::label('level', 'Job Level')}}
-                <select name="level" id="">
+            <div class="form-group ml-2">
+                {{-- {{Form::label('level', 'Job Level')}} --}}
+                <select name="level" id="" class="selectpicker border rounded border-secondary">
                     @foreach ($data['levels'] as $level) 
                         <option value="{{$level->id}}" @if ($level->id === $data['job_post']['level_id']) selected @endif>{{$level->job_level}}</option>
                         {{-- @if ($level->id === $data['job_post']['level_id']) 
@@ -44,7 +44,7 @@
                 </select>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group my-2">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', $data['job_post']['title'], ['class' => 'form-control', 'placeholder' => 'Enter your job title here'])}}
             {{Form::label('description', 'Description')}}
