d2cced2c3bd8e58583696b30f3ca4a4c5cd0fa49
Reginald Diocera <rmdiocera@gmail.com>
2020-06-19 20:02:07 +0800

    Added DataTables CSS and script links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,11 @@
     <!-- (Optional) Latest compiled and minified JavaScript translation files -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- DataTables -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" defer></script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js" defer></script>   
     
     <!-- CKEditor 4 -->
     <script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
@@ -29,8 +34,6 @@
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     {{-- Bootstrap Select --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
-    <!-- DataTables -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
     {{-- Emoji CSS --}}
     <link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet">
 
@@ -42,17 +45,13 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        @include('inc.navbar')
+    @include('inc.navbar')
         <div class="container mt-4">
             @include('inc.alerts')
             @include('inc.modals')
             @yield('content')
-            @include('components.who')
             @include('inc.footer')
         </div>
-    </div>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" defer></script>
     @include('inc.script');
 </body>
 </html>
