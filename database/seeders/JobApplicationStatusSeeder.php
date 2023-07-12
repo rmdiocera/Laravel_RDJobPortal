c79c00f8ca76add0dd16fbf323a04c57937ecc74
Regi Diocera <rmdiocera@gmail.com>
2023-07-12 19:00:09 +0800

    Status seeder fix

--- database/seeders/JobApplicationStatusSeeder.php
+++ database/seeders/JobApplicationStatusSeeder.php
@@ -14,10 +14,10 @@ class JobApplicationStatusSeeder extends Seeder
      */
     public function run()
     {
-        $emp_types = ['Applied', 'Invited', 'Rejected', 'Accepted', 'Declined'];
+        $statuses = ['Applied', 'Invited', 'Rejected', 'Accepted', 'Declined'];
 
-        foreach ($emp_types as $emp_type) {
-            JobApplicationStatus::create(['emp_type' => $emp_type]);
+        foreach ($statuses as $status) {
+            JobApplicationStatus::create(['status' => $status]);
         }
     }
 }
